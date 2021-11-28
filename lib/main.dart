5be58da40c04c74fd40416a7efff0b9a8de738bf
farsi losa werdiansyah <farsi.werdiansyah@aruna.id>
2021-11-28 09:14:35 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:dzikirapp/pages/index.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAHAsf51D0A407EklG1bs-5wA7EbyfNFg0',
+      appId: 'dzikir-app',
+      messagingSenderId: '448618578101',
+      projectId: 'react-native-firebase-testing',
+    ),
+  );
   runApp(MyApp());
 }
 
